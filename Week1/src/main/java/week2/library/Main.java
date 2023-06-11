081ef0e4d6427edda42d81e70d5b5458fac07312
Hanna Mikulich <mikulichhanna@gmail.com>
2023-06-11 16:13:05 +0300

    finished method of taking books

--- Week1/src/main/java/week2/library/Main.java
+++ Week1/src/main/java/week2/library/Main.java
@@ -15,60 +15,52 @@ public class Main {
             Scanner scanner = new Scanner(System.in);
             String fromRegisterOrLogin = scanner.nextLine();
 
-            if (fromRegisterOrLogin.equals("1")) {
+            if (fromRegisterOrLogin.equals("1")) {               //registration
                 cardNumber++;
-                System.out.println("Please, fill out a form. Enter your name: ");
+                System.out.print("Please, fill out the form. Enter your name: ");
                 String nameFromScanner = scanner.nextLine();
-                System.out.println("Enter your faculty: ");
+                System.out.print("Enter your faculty: ");
                 String facultyFromScanner = scanner.nextLine();
-                System.out.println("Enter your date of Birth in format: dd.mm.yyyy ");
+                System.out.print("Enter your date of Birth: ");
                 String dateOfBirth = scanner.nextLine();
-                System.out.println("Enter your phoneNumber");
+                System.out.print("Enter your phoneNumber:");
                 String phoneNumber = scanner.nextLine();
                 readerList.add(new Reader(nameFromScanner, cardNumber, facultyFromScanner,
                         dateOfBirth, phoneNumber));
             }
-            System.out.println("To login enter your full name:");
+            System.out.print("To login enter your full name:");
             String enterLoginFromConsole = scanner.nextLine();
-//            boolean listContainsReader = readerList.stream()
-//                    .anyMatch(reader -> (reader.getName().equalsIgnoreCase(enterLoginFromConsole)));
 
-            Reader findedReader = readerList.stream()
+            Reader findedReader = readerList.stream()                         //find reader in readerList
                     .filter(reader -> (reader.getName().equalsIgnoreCase(enterLoginFromConsole)))
                     .findAny()
                     .orElse(null);
             if (findedReader == null) {
-                System.out.println("Sorry, no user named " + enterLoginFromConsole);
+                System.out.println("Sorry, NO user named " + enterLoginFromConsole);
             }else {
                 System.out.println(" Take books - enter '1'. \n Return books - enter '2'." +
-                        "\n Finish work - enter any other symbol");
+                        "\n Finish work - enter any other symbol.");
                 String chosenNumber = scanner.nextLine();
                 if (chosenNumber.equals("1")) {
-                    System.out.println("How many books do you want?");
+                    System.out.println("How many books do you want to take?");
                     int amountOfBooksFromConsole = scanner.nextInt();
                     findedReader.takeBook(amountOfBooksFromConsole);
                     String [] nameOfBooksFromConsole = new String[amountOfBooksFromConsole];
-
-                    System.out.println("Enter the name of book(s): ");
+                    List <Book> booksToTake = new ArrayList<>();
                     for (int i = 0; i < amountOfBooksFromConsole; i++) {
-                       // String booksFromConsole = scanner.nextLine();
-                        nameOfBooksFromConsole[i] = scanner.nextLine();
+                        System.out.print("Enter the name of book(s): ");
+                        String booksFromConsole = scanner.next();
+                        nameOfBooksFromConsole[i] = booksFromConsole;
+                        booksToTake.add(new Book(booksFromConsole, "author"+i));
                     }
                     findedReader.takeBook(nameOfBooksFromConsole);
-
-
-                }
-//                switch (chosenNumber){
-//                    case "1": reader.takeBook();
-//                    case "2": reader.returnBook();
-//                    case "3": continue;
-//                    default: break;
-//                       }
-
+                    findedReader.takeBook(booksToTake);
+                    System.out.println(findedReader);
                 }
 
-            readerList.forEach(s -> System.out.println(s));
-            System.out.println("== Thank you for visiting our Library! ==");
+            }
+           // readerList.forEach(s -> System.out.println(s));
+            System.out.println("== Thank you for visiting our Library! ==\n");
 
             //readerList.forEach( s -> s.getName().equals(fromRegisterOrLogin) );
             // scanner.close();
